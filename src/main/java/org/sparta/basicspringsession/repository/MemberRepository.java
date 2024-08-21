f33d666c48082e7fb8be87671888b9259a30341a
tiyu25 <tiyuuuu9@email.com>
2024-08-21 17:53:03 +0900

    기본 구조 세팅

---
+++ src/main/java/org/sparta/basicspringsession/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package org.sparta.basicspringsession.repository;
+
+import org.sparta.basicspringsession.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
---
+++ src/main/java/org/sparta/basicspringsession/service/MemberService.java
@@ -0,0 +1,10 @@
+package org.sparta.basicspringsession.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
